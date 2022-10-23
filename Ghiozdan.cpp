12bb789753bba9702855f436813ca53ce8577d6f
Antonia Ungureanu <93488180+uantoniaa@users.noreply.github.com>
2022-10-24 00:03:22 +0300

    Create Ghiozdan.cpp

---
+++ Ghiozdan.cpp
@@ -0,0 +1,33 @@
+#include "Ghiozdan.h"
+
+Ghiozdan::Ghiozdan(int pret, char marime, int culoare) {
+
+}
+void Ghiozdan::setPret(int pret) {
+    Pret = pret;
+}
+int Ghiozdan::getPret() {
+    return Pret;
+}
+void Ghiozdan::setMarime(char marime) {
+    Marime = marime;
+}
+
+char Ghiozdan::getMarime() {
+    return Marime;
+}
+
+void Ghiozdan::setCuloare(int culoare) {
+    Culoare = culoare;
+}
+
+int Ghiozdan::getCuloare() {
+    return Culoare;
+}
+
+ostream &operator<<(ostream &COUT, Ghiozdan &Ghiozdan1) {
+    COUT <<"Marime ->"<<Ghiozdan1.getMarime();
+    COUT <<"Pret ->"<<Ghiozdan1.getPret();
+    COUT <<"Culoare ->"<<Ghiozdan1.getCuloare(); 
+    return COUT;
+}
