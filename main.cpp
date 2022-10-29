50408c44a12a7d8ceda795c463b1b97407308112
Antonia Ungureanu <93488180+uantoniaa@users.noreply.github.com>
2022-10-29 23:06:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
-#include "Acuarele.h"
-#include "Caiet.h"
-#include "Ghiozdan.h"
-#include "Pix.h"
+
 int main() {
-	Acuarele1(20, 10);
-    	Caiet1(5, A4);
-    	Ghiozdan1(80, copii, roz);
-	Pix1(5, mic, alb);
+
     	return 0;
 }
