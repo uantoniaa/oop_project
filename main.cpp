480d7121d1456b5b5a871946bd06b5a9c875bce2
Antonia Ungureanu <93488180+uantoniaa@users.noreply.github.com>
2022-10-29 22:55:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main() {
 	Acuarele Acuarele1(20, 10);
-    Caiet Caiet1(5, A4);
-    Ghiozdan Ghiozdan1(80, copii, roz);
+    	Caiet Caiet1(5, A4);
+    	Ghiozdan Ghiozdan1(80, copii, roz);
 	Pix Pix1(5, mic, alb);
-    return 0;
+    	return 0;
 }
